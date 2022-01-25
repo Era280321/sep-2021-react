6f1468c609f96b2e501379099466859e93760b8b
IrynaBozhyk <bozhykiryna@gmail.com>
2022-01-25 21:01:32 +0200

    testing hw2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import Users from "./components/Users/Users";
+import Posts from "./components/Posts/Posts";
+import UserDetails from "./components/UserDetails/UserDetails";
+import css from 'App.module.css'
 
 function App() {
   return (
-    <div className="App">
-    <h1>Hello</h1>
+    <div>
+      <div className={css.wrap}>
+        <Users/>
+        <UserDetails/>
+      </div>
+      <Posts/>
     </div>
   );
 }
